b947a3b78fab2038d5b3bb360e9498090fd58830
samssh <61908517+samssh@users.noreply.github.com>
2020-07-02 14:35:15 +0430

    make SendRequest

--- src/main/java/ir/nargil/Token.java
+++ src/main/java/ir/nargil/Token.java
@@ -1,8 +1,21 @@
 package ir.nargil;
 
+import com.fasterxml.jackson.core.type.*;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
+import java.util.Map;
+
 public class Token extends SendRequest{
     public Token() {
         super("/Authentication/SignIn");
         request.addHeader("Authorization", Constants.basicAuth);
+    }
+
+    public String getToken() throws IOException {
+        ObjectMapper mapper = new ObjectMapper();
+        Map<String,Object> values =  mapper.readValue(getResponse(),new TypeReference<Map<String, Object>>(){});
+        Map<String,Object> dataJson = (Map<String, Object>) values.get("data");
+        return (String) dataJson.get("Token");
     }
 }
